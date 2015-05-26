f12f704faa1add38e5e082f359d587b898b2aad2
Nicholas Stock <stockn@gmail.com>
2015-05-26 17:59:41 +0200

    user 5.3 compatible syntax

--- tests/integration/AuthenticationTest.php
+++ tests/integration/AuthenticationTest.php
@@ -14,20 +14,20 @@ class AuthenticationTest extends LocalWebTestCase {
 		return parent::tearDown();
 	}
 	public function testLogin() {
-		\User::create(['username' => "nicholasjstock", "email" => "stockn@gmail.com", "first_name" => "nicholas", "last_name" => "stock", "password" => "surfer"]);
-		$parameters = array('user' => ['username' => 'nicholasjstock', "password" => "surfer"]);
+		\User::create(array('username' => "nicholasjstock", "email" => "stockn@gmail.com", "first_name" => "nicholas", "last_name" => "stock", "password" => "surfer"));
+		$parameters = array('user' => array('username' => 'nicholasjstock', "password" => "surfer"));
 		$this->client->post('/users/sign_in', $parameters);
 		$this->assertEquals(200, $this->client->response->status());
 	}
 	public function testProtectedRoute() {
-		\User::create(['auth_token' => "thoken", 'username' => "nicholasjstock", "email" => "stockn@gmail.com", "first_name" => "nicholas", "last_name" => "stock", "password" => "surfer"]);
+		\User::create(array('auth_token' => "thoken", 'username' => "nicholasjstock", "email" => "stockn@gmail.com", "first_name" => "nicholas", "last_name" => "stock", "password" => "surfer"));
 		$header = 'Token token="thoken", user_email="stockn@gmail.com"';
-		$this->client->get("/api/speakers", [], ["HTTP_Authorization" => $header]);
+		$this->client->get("/api/speakers", array(), array("HTTP_Authorization" => $header));
 		$this->assertEquals(200, $this->client->response->status());
 	}
 	public function testProtectedRouteWrongCredentials() {
 		$header = 'Token token="thoken", user_email="stockn@gmail.com"';
-		$this->client->get("/api/speakers", [], ["HTTP_Authorization" => $header]);
+		$this->client->get("/api/speakers", array(), array("HTTP_Authorization" => $header));
 		$this->assertEquals(403, $this->client->response->status());
 	}
 
